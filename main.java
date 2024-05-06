ffdf3a72b454d9bc56f74e261e159c6da6c93a56
Abdalla Omar <152969832+Abdalla-00@users.noreply.github.com>
2024-05-06 16:24:56 -0400

    Update main.java

--- main.java
+++ main.java
@@ -5,13 +5,3 @@ public class main {
         game.play();
     }
 }
-
-/**
- * Questions
- * I changed my getLongDescription function to now have a parameter instead of
- * adjusting the room
- * I also am not using the static function for getRooms in Room.java, rather
- * made the ArrayList<Room rooms static and protected so it can be accessed by
- * the entire class.
- * 
- */
